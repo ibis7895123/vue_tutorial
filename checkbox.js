d7d2397926962127e0c72061fc68ad1ec633a679
tokai235 <the.summer.of.spiritual@gmail.com>
2021-11-21 16:51:34 +0900

    スタイルの継承ロジックの確認

--- checkbox.js
+++ checkbox.js
@@ -1,23 +1,24 @@
-Vue.component('base-checkbox', {
+Vue.component("base-checkbox", {
   model: {
-    prop: 'checked',
-    event: 'change',
+    prop: "checked",
+    event: "change",
   },
   props: {
     checked: Boolean,
   },
   template: `
     <input
+      class="foo bar"
       type="checkbox"
       v-bind:checked='checked'
       v-on:change="$emit('change', $event.target.checked)"
     >
-  `
-})
+  `,
+});
 
 const app = new Vue({
-  el: '#checkbox',
+  el: "#checkbox",
   data: {
     check: false,
   },
-})
+});
